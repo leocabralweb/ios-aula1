ff5426abe75f2f9f59f3ab79705589e06c5bdd7d
ios2749 <ios2749@Mac-mini-de-Caelum-47.local>
2012-06-26 19:01:31 -0300

    Initial Commit

---
+++ 01aMundo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  01aMundo
+//
+//  Created by ios2749 on 26/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ 01aMundo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  01aMundo
+//
+//  Created by ios2749 on 26/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
