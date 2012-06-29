5bf64c8f8842a18d31c842c110c02cdc85464487
Leonardo Cabral <leogweb@gmail.com>
2012-06-29 18:19:25 -0300

    commit inicial

--- 01aMundo/ViewController.h
+++ 01aMundo/ViewController.h
@@ -10,4 +10,33 @@
 
 @interface ViewController : UIViewController
 
+    // Hello World
+    @property IBOutlet UILabel *meuLabel;
+    @property IBOutlet UITextField *meuTexto;
+    
+    -(IBAction) atualizaLabel:(id)sender;
+
+    // Soma
+    @property IBOutlet UITextField *somar1;
+    @property IBOutlet UITextField *somar2;
+    @property IBOutlet UILabel *resultadoSoma;
+
+    @property IBOutlet UISlider *slider1;
+    @property IBOutlet UISlider *slider2;
+
+    -(IBAction) soma:(id)sender;
+
+    -(IBAction) valorAtualizado1:(id)sender;
+    -(IBAction) valorAtualizado2:(id)sender;
+    -(IBAction) valueToSlide1:(id)sender;
+    -(IBAction) valueToSlide2:(id)sender;
+
+    -(void) atualizaCampoSoma:(UITextField *)target withValue:(float)value;
+    -(void) atualizaSliderSoma:(UISlider *)slider withValue:(float)value;
+
+
+    // Tabuada
+    @property IBOutlet UITextField *numero;
+    -(IBAction) tabuada:(id)sender;
+
 @end
--- 01aMundo/ViewController.m
+++ 01aMundo/ViewController.m
@@ -14,6 +14,69 @@
 
 @implementation ViewController
 
+@synthesize meuLabel, meuTexto;
+@synthesize somar1, somar2, resultadoSoma, slider1, slider2;
+@synthesize numero;
+
+
+-(IBAction)atualizaLabel:(id)sender 
+{
+    [meuLabel setText:[meuTexto text]];
+    NSLog(@"digitado pelo usuário: %@", [meuTexto text]);
+}
+
+
+-(IBAction)soma:(id)sender
+{
+    float resultado = [[somar1 text] floatValue] + [[somar2 text] floatValue];
+    //NSString *valor = [NSString stringWithFormat:@"%d", resultado];
+    //[resultadoSoma setText:valor];
+    
+    [resultadoSoma setText:[NSString stringWithFormat:@"%f", resultado]];
+}
+
+-(IBAction)valorAtualizado1:(id)sender
+{
+    [self atualizaCampoSoma:somar1 withValue:[slider1 value]];
+}
+
+-(IBAction)valorAtualizado2:(id)sender
+{
+    [self atualizaCampoSoma:somar2 withValue:[slider2 value]];
+}
+
+-(IBAction)valueToSlide1:(id)sender
+{
+    [self atualizaSliderSoma:slider1 withValue:[[sender text] floatValue]];
+}
+
+-(IBAction)valueToSlide2:(id)sender
+{
+    [self atualizaSliderSoma:slider2 withValue:[[sender text] floatValue]];
+}
+
+-(void) atualizaCampoSoma:(UITextField *)target withValue:(float)value
+{
+    [target setText:[NSString stringWithFormat:@"%f", value]];
+}
+
+-(void)atualizaSliderSoma:(UISlider *)slider withValue:(float)value
+{
+    [slider setValue: value];
+}
+
+-(IBAction)tabuada:(id)sender
+{
+    int numeroBase = [[numero text] intValue];
+    
+    for (int mult=1; mult<=10; mult++) {
+        int result = numeroBase * mult;
+        NSLog(@"Tabuada %d x %d = %d", numeroBase, mult, result);
+    }
+}
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
